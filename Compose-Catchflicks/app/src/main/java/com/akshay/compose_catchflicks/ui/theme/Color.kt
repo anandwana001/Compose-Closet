3d6e4149138e88fe324970c9c496fd8095dc5f07
anandwana001 <akshaynandwana001@gmail.com>
2022-07-10 17:42:18 +0530

    init project

---
+++ Compose-Catchflicks/app/src/main/java/com/akshay/compose_catchflicks/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.akshay.compose_catchflicks.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFF303133)
+val Purple500 = Color(0xFF303133)
+val Purple700 = Color(0xFF07080c)
+val Teal200 = Color(0xFFffbc03)
