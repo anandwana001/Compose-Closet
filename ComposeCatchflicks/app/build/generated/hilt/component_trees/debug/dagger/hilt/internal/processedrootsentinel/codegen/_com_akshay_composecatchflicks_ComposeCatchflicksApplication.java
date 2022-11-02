833a7cad3a2b205b3abcc300c19d3f80202779f0
anandwana001 <akshaynandwana001@gmail.com>
2022-11-02 12:04:42 +0530

    remove mistake files

--- ComposeCatchflicks/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_akshay_composecatchflicks_ComposeCatchflicksApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.akshay.composecatchflicks.ComposeCatchflicksApplication"
-)
-public final class _com_akshay_composecatchflicks_ComposeCatchflicksApplication {
-}
